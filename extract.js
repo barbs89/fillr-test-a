c1aad432310d7e78e39c247699ddc4cc32c4be39
Barbara Rodriguez <barbs2089@gmail.com>
2022-05-09 00:27:04 +1000

    Add JSON.stringify method to copy and paste output to mapping.json

--- extract.js
+++ extract.js
@@ -2,13 +2,13 @@ module.exports.extract = function(window) {
 
   // Create JSON object
   const data = {};
+
   // Array of strings (form titles)
   const formTitle = ['Amazon commerce fields', 'RFC3106 ECLM eCommerce fields'];
   let arrElements = [];
   let objKeys = [];
   let objValues = [];
 
-  
   // Find the form tag within the html document 
   const forms = window.document.getElementsByTagName('form');
 
@@ -24,9 +24,7 @@ module.exports.extract = function(window) {
       }
     }
   }
-
   findForm(forms)
-
 
   // Iterate through array of form elements and extract form attribute name and 'td' values
   for (let i = 0; i < arrElements[0].length; i++) {
@@ -34,19 +32,20 @@ module.exports.extract = function(window) {
     objValues.push(objValue = arrElements[0].item(i).parentNode.previousSibling.innerHTML.trim());
   }
 
-  // Working key value that matches README instructions
-  // eg: "Ecom_ShipTo_Postal_Name_Prefix":"ship to title"
-  
+  // Working key value that matches README instructions eg: "Ecom_ShipTo_Postal_Name_Prefix":"ship to title"
   // objKeys.forEach((element, index) => {
   //   data[element] = objValues[index];
   // });
 
-   // Working key value that passes TESTS
+  // Working key value that passes TESTS
   // Iterate objValues with forEach method to push key value pair to object
   objValues.forEach((element, index) => {
     data[element] = objValues[index] + " " + objKeys[index];
   });
 
-  return data;
+  // stringify JSON to copy and paste data to mapping.json
+  console.log(JSON.stringify(data));
+
+  return data
   
 }
